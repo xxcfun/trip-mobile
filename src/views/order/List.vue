c9b5c6901636ba15009217184d3faab9a6aef2da
hhdMrLion <55070348+hhdMrLion@users.noreply.github.com>
2021-05-27 15:20:17 +0800

    '删除订单接口联调'

--- src/views/order/List.vue
+++ src/views/order/List.vue
@@ -13,7 +13,7 @@
     </van-tabs>
     <!-- 订单记录 -->
     <div class="order-list">
-      <div class="order-item" v-for="item in orderList" :key="item.sn">
+      <div class="order-item" v-for="item in orderList" :key="item.sn" v-show="item.sn">
         <div class="order-head">
           <div class="order-num">订单号：{{ item.sn }}</div>
           <div class="order-status text-warning">{{ constants.ORDER_STATUS[item.status] }}</div>
@@ -30,9 +30,11 @@
         <div class="order-footer">
           <div>总共{{ item.buy_count }}件商品 合计 ￥{{ item.buy_amount }}</div>
           <van-button round size="small" type="warning"
-                      v-if="item.status == constants.ORDER_STATUS_PAY"
-                      @click="goPay(item)">去支付</van-button>
-          <van-button round size="small" type="warning">删除订单</van-button>
+            v-if="item.status == constants.ORDER_STATUS_PAY"
+            @click="goPay(item)">去支付</van-button>
+          <van-button round size="small" type="warning"
+            v-if="item.status == constants.ORDER_STATUS_DONE || item.status == constants.ORDER_STATUS_CANCEL"
+            @click="deleteOrder(item)">删除订单</van-button>
           <van-button round size="small" type="info">订单详情</van-button>
         </div>
       </div>
@@ -98,6 +100,30 @@
        */
       goPay (item) {
         this.$router.push({ name: 'OrderPay', params: { sn: item.sn } })
+      },
+      /**
+       * 订单删除
+       */
+      deleteOrder (item) {
+        // 弹框确认
+        this.$dialog.alert({
+          title: '温馨提示',
+          message: '删除订单将无法恢复，确认删除订单'
+        }).then(() => {
+          // 调用接口
+          const url = OrderApis.orderDetailUrl.replace('#{sn}', item.sn)
+          ajax.delete(url).then(res => {
+            // 告诉用户已经删除成功
+            // 隐藏界面上的显示
+            if (res.status === 201) {
+              this.$notify({
+                type: 'success',
+                message: '删除成功'
+              })
+              item.sn = ''
+            }
+          })
+        })
       }
     },
     mounted () {
